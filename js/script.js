3135c5fce3e17acfac490a1ff1e61cc2919dc158
Emeka <frankeokafor@gmail.com>
2025-05-08 06:29:08 +0100

    addition of some details

--- js/script.js
+++ js/script.js
@@ -52,175 +52,182 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Portfolio filter
 
-
-
-
-
 // CONTACT
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sendToGmailButton = document.getElementById("sendToGmail");
+
+  if (sendToGmailButton) {
+    sendToGmailButton.addEventListener("click", function () {
+      const form = document.getElementById("contactForm");
+      const nameInput = form.querySelector("#name");
+      const emailInput = form.querySelector("#email");
+      const subjectInput = form.querySelector("#subject");
+      const messageInput = form.querySelector("#message");
+      const recipient = "okoronkwonmesoma@gmail.com"; // Replace with your Gmail address
+
+      const name = nameInput ? nameInput.value : "";
+      const email = emailInput ? emailInput.value : "";
+      const subject = subjectInput ? subjectInput.value : "";
+      const message = messageInput ? messageInput.value : "";
+
+      const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0ASubject: ${subject}%0D%0AMessage:%0D%0A${message}`;
+      const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(
+        subject
+      )}&body=${body}`;
+
+      window.location.href = mailtoLink;
+    });
+  } else {
+    console.error("Button with ID 'sendToGmail' not found.");
+  }
+});
+
+(function ($) {
+  "use strict";
+
+  // loader
+  var loader = function () {
+    setTimeout(function () {
+      if ($("#loader").length > 0) {
+        $("#loader").removeClass("show");
+      }
+    }, 1);
+  };
+  loader();
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 200) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").addClass("nav-sticky");
+    } else {
+      $(".navbar").removeClass("nav-sticky");
+    }
+  });
+
+  // Smooth scrolling on the navbar links
+  $(".navbar-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      $("html, body").animate(
+        {
+          scrollTop: $(this.hash).offset().top - 45,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+
+      if ($(this).parents(".navbar-nav").length) {
+        $(".navbar-nav .active").removeClass("active");
+        $(this).closest("a").addClass("active");
+      }
+    }
+  });
+
+  // Typed Initiate
+  if ($(".hero .hero-text h2").length == 1) {
+    var typed_strings = $(".hero .hero-text .typed-text").text();
+    var typed = new Typed(".hero .hero-text h2", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
+    });
+  }
+
+  Skills;
+  $(".skills").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
+
+  // Testimonials carousel
+  $(".testimonials-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+
+  // Portfolio filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+
+  $("#portfolio-filter li").on("click", function () {
+    $("#portfolio-filter li").removeClass("filter-active");
+    $(this).addClass("filter-active");
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+})(jQuery);
+
+// Navbar collapsing
+
+
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    const sendToGmailButton = document.getElementById('sendToGmail');
+  const navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('#navbarSupportedContent');
 
-    if (sendToGmailButton) {
-        sendToGmailButton.addEventListener('click', function() {
-            const form = document.getElementById('contactForm');
-            const nameInput = form.querySelector('#name');
-            const emailInput = form.querySelector('#email');
-            const subjectInput = form.querySelector('#subject');
-            const messageInput = form.querySelector('#message');
-            const recipient = 'okoronkwonmesoma@gmail.com'; // Replace with your Gmail address
-
-            const name = nameInput ? nameInput.value : '';
-            const email = emailInput ? emailInput.value : '';
-            const subject = subjectInput ? subjectInput.value : '';
-            const message = messageInput ? messageInput.value : '';
-
-            const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0ASubject: ${subject}%0D%0AMessage:%0D%0A${message}`;
-            const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${body}`;
-
-            window.location.href = mailtoLink;
-        });
-    } else {
-        console.error("Button with ID 'sendToGmail' not found.");
-    }
+  navLinks.forEach(navLink => {
+    navLink.addEventListener('click', (event) => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarCollapse.classList.remove('show');
+        event.target.blur(); // Remove focus from the clicked link
+      }
+    });
+  });
 });
 
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
+  const navbarToggler = document.querySelector('.navbar-toggler');
+  const navbarCollapse = document.querySelector('#navbarSupportedContent');
 
-
-(function ($) {
-  "use strict";
-  
-  // loader
-  var loader = function () {
-      setTimeout(function () {
-          if ($('#loader').length > 0) {
-              $('#loader').removeClass('show');
-          }
-      }, 1);
-  };
-  loader();
-  
-  
-  // Initiate the wowjs
-  new WOW().init();
-  
-  
-  // Back to top button
-  $(window).scroll(function () {
-      if ($(this).scrollTop() > 200) {
-          $('.back-to-top').fadeIn('slow');
-      } else {
-          $('.back-to-top').fadeOut('slow');
+  navLinks.forEach(navLink => {
+    navLink.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarCollapse.classList.remove('show');
       }
+    });
   });
-  $('.back-to-top').click(function () {
-      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-      return false;
-  });
-  
-  
-  // Sticky Navbar
-  $(window).scroll(function () {
-      if ($(this).scrollTop() > 0) {
-          $('.navbar').addClass('nav-sticky');
-      } else {
-          $('.navbar').removeClass('nav-sticky');
-      }
-  });
-  
-  
-  // Smooth scrolling on the navbar links
-  $(".navbar-nav a").on('click', function (event) {
-      if (this.hash !== "") {
-          event.preventDefault();
-          
-          $('html, body').animate({
-              scrollTop: $(this.hash).offset().top - 45
-          }, 1500, 'easeInOutExpo');
-          
-          if ($(this).parents('.navbar-nav').length) {
-              $('.navbar-nav .active').removeClass('active');
-              $(this).closest('a').addClass('active');
-          }
-      }
-  });
-  
-  
-  // Typed Initiate
-  if ($('.hero .hero-text h2').length == 1) {
-      var typed_strings = $('.hero .hero-text .typed-text').text();
-      var typed = new Typed('.hero .hero-text h2', {
-          strings: typed_strings.split(', '),
-          typeSpeed: 100,
-          backSpeed: 20,
-          smartBackspace: false,
-          loop: true
-      });
-  }
-  
-  
-  Skills
-  $('.skills').waypoint(function () {
-      $('.progress .progress-bar').each(function () {
-          $(this).css("width", $(this).attr("aria-valuenow") + '%');
-      });
-  }, {offset: '80%'});
-
-
-  // Testimonials carousel
-  $(".testimonials-carousel").owlCarousel({
-      center: true,
-      autoplay: true,
-      dots: true,
-      loop: true,
-      responsive: {
-          0:{
-              items:1
-          }
-      }
-  });
-  
-  
-  
-  // Portfolio filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-  });
-
-  $('#portfolio-filter li').on('click', function () {
-      $("#portfolio-filter li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-      portfolioIsotope.isotope({filter: $(this).data('filter')});
-  });
-  
-})(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
